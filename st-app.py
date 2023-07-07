7e82b5e5bc51f5d61aea66f5dd55ebcaf1d68c4e
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-07 13:49:16 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -8,19 +8,19 @@ fig_gini_esc = px.box(df, x='gini', y='i_escolaridade')
 
 
 st.title('DASHBOARD IDH')
-texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel, Enio'
+texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel Duarte, Enio Kilder'
 st.text(texto1)
 tab1, tab2, tab3 = st.tabs(["O que é IDH?", "GINI e Escolaridade", "Tabela de dados"])
 
 with tab1:
-   st.text("**O que representa o Índice de Desenvolvimento Humano - IDH**")
+   st.markdown("**O que representa o Índice de Desenvolvimento Humano - IDH**")
    st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países\nnos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros,\ncom o intuito de avaliar o bem-estar de uma população")
    st.write("fonte: https://encurtador.com.br/eQS35")
 
 with tab2:
-   st.text("**GINI** **e** **escolaridade**")
+   st.markdown("**GINI e escolaridade**")
    st.plotly_chart(fig_gini_esc)
 
 with tab3:
-   st.text("**Tabela** **base**")   
+   st.markdown("**Tabela base**")   
    df
