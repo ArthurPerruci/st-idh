5c54beae951156be5eac70b088814986ff5956ef
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-07 11:01:01 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -1,23 +1,26 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("atlas.csv")
+fig_gini_esc = px.box(df, x='gini', y='i_escolaridade')
+
+
 
 st.title('DASHBOARD IDH')
 texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel, Enio'
 st.text(texto1)
-tab1, tab2, tab3 = st.tabs(["O que é IDH?", "Dog", "Tabela de dados"])
+tab1, tab2, tab3 = st.tabs(["O que é IDH?", "GINI e Escolaridade", "Tabela de dados"])
 
 with tab1:
-   st.subheader("O que representa o Índice de Desenvolvimento Humano - IDH")
+   st.text("**O que representa o Índice de Desenvolvimento Humano - IDH**")
    st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países\nnos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros,\ncom o intuito de avaliar o bem-estar de uma população")
    st.write("fonte: https://encurtador.com.br/eQS35")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=50)
 
 with tab2:
-   st.subheader("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   st.text("**GINI e escolaridade**")
+   fig_gini_esc.show()
 
 with tab3:
-   st.subheader("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-   df = pd.read_csv("atlas.csv")
+   st.text("**Tabela base**")   
    df
