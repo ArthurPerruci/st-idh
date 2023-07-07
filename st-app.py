25f31f340211605f230e08d7d6cacd44ff69ec74
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-07 10:51:29 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -4,11 +4,11 @@ import pandas as pd
 st.title('DASHBOARD IDH')
 texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel, Enio'
 st.text(texto1)
-tab1, tab2, tab3 = st.tabs(["IDH", "Dog", "Tabela de dados"])
+tab1, tab2, tab3 = st.tabs(["O que é IDH?", "Dog", "Tabela de dados"])
 
 with tab1:
    st.subheader("O que representa o Índice de Desenvolvimento Humano - IDH")
-   st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países nos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros, com o intuito de avaliar o bem-estar de uma população")
+   st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países\nnos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros,\ncom o intuito de avaliar o bem-estar de uma população")
    st.write("fonte: https://encurtador.com.br/eQS35")
    st.image("https://static.streamlit.io/examples/cat.jpg", width=50)
 
