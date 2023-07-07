07fadd565d55cdc3c7127654b270a0419bfb047c
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-06 22:28:26 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -1,2 +1,7 @@
 import streamlit as st
 st.write('DASH BOARD IDH')
+import altair as alt
+import pandas as pd
+from vega_datasets import data
+
+df = pd.read_csv("/content/gdrive/MyDrive/ColabCSV/atlas.csv")
