7009946c80705a913370c0b7a6937082f9440204
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-07 09:04:13 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 st.title('DASHBOARD IDH - EQUIPE 12')
 st.write('DASH BOARD IDH')
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Tabela de dados"])
 
-df = pd.read_csv("atlas.csv")
-df
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+   df = pd.read_csv("atlas.csv")
+   df
