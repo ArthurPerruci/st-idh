c35224516aa9ab5b172d3570cfbc7f202ab8c1e2
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-07 10:48:32 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -3,21 +3,21 @@ import pandas as pd
 
 st.title('DASHBOARD IDH')
 texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel, Enio'
-st.write(texto1)
+st.text(texto1)
 tab1, tab2, tab3 = st.tabs(["IDH", "Dog", "Tabela de dados"])
 
 with tab1:
-   st.header("O que representa o Índice de Desenvolvimento Humano - IDH")
-   st.write("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países nos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros, com o intuito de avaliar o bem-estar de uma população")
-   st.write("fonte: https://encurtador.com.br/eQS35")
+   st.subheader("O que representa o Índice de Desenvolvimento Humano - IDH")
+   st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países nos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros, com o intuito de avaliar o bem-estar de uma população")
+   st.text("fonte: https://encurtador.com.br/eQS35")
    st.image("https://static.streamlit.io/examples/cat.jpg", width=50)
 
 with tab2:
-   st.header("A dog")
+   st.subheader("A dog")
    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
 with tab3:
-   st.header("An owl")
+   st.subheader("An owl")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
    df = pd.read_csv("atlas.csv")
    df
