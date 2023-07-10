18c27e2f534c286047fd563f517a8148e269ae06
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-10 10:34:53 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -13,18 +13,22 @@ fig_gini_esc = px.box(df, x='gini', y='i_escolaridade')
 st.title('DASHBOARD IDH')
 texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel Duarte, Enio Kilder'
 st.text(texto1)
-tab1, tab2, tab3, tab4 = st.tabs(["O que é IDH?", "Faixas de IDH", "GINI e Escolaridade", "Tabela de dados"])
+tab1, tab2, tab3, tab4 = st.tabs(["Entender o IDH", "ou", "GINI e Escolaridade", "Tabela de dados"])
 
 with tab1:
-   st.markdown("**O que representa o Índice de Desenvolvimento Humano - IDH**")
-   st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países\nnos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros,\ncom o intuito de avaliar o bem-estar de uma população")
-   st.write("fonte: IPEA / Desafios do Desenvolvimento ( https://encurtador.com.br/eQS35 )")
-   st.image(idh_composicao)
+   col1, col2 = st.columns(2)
+      with col1:
+         st.markdown("**O que representa o Índice de Desenvolvimento Humano - IDH**")
+         st.text("O Índice de Desenvolvimento Humano (IDH) compara indicadores de países\nnos itens riqueza, alfabetização, educação, esperança de vida, natalidade e outros,\ncom o intuito de avaliar o bem-estar de uma população")
+         st.write("fonte: IPEA / Desafios do Desenvolvimento ( https://encurtador.com.br/eQS35 )")
+         st.image(idh_composicao)
+      with col2:
+         st.write("O valor do IDH varia de 0 (zero) a 1, e conforme o valor pode ser considerado de muito baixo a muito alto conforme as faxias descritas na figura.")
+         st.image(idh_faixas)
+         st.write("Reproduzido de IPEA / O Índice de Desenvolvimento Humano Municipal Brasileiro (disponível para download em: https://repositorio.ipea.gov.br/handle/11058/2375) ")
 
 with tab2:
-   st.write("O valor do IDH varia de 0 (zero) a 1, e conforme o valor pode ser considerado de muito baixo a muito alto conforme as faxias descritas na figura.")
-   st.image(idh_faixas)
-   st.write("Reproduzido de IPEA / O Índice de Desenvolvimento Humano Municipal Brasileiro (disponível para download em: https://repositorio.ipea.gov.br/handle/11058/2375) ")
+   st.write('ou')
 
 with tab3:
    st.markdown("**GINI e escolaridade**")
