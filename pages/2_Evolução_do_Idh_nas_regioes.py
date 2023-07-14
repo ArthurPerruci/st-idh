43cafa9487606003bc5fa7432a8756d7ed40ef52
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-14 13:24:19 -0300

    Update 2_Evolução_do_Idh_nas_regioes.py

--- pages/2_Evolução_do_Idh_nas_regioes.py
+++ pages/2_Evolução_do_Idh_nas_regioes.py
@@ -30,7 +30,6 @@ df['regiao'] = df['uf'].apply(check_regiao)
 st.set_page_config(page_title="Evolução IDH nas regiões", page_icon=":📈:")
 
 st.title('A evolução do IDH nas regiões brasileiras no período de 1991 a 2010')
-st.markdown("O gráfico mostra a evolução dos valores médios de idhm das cinco regiões brasileiras. Fica evidente um processo de incremento dos valores de idh entre 1991 e 2010. Percebe-se também que os valores de idh nas regiões norte e nordeste são discretamente inferiores que os das demais regiões, da mesma maneira precebe-se que esta diferença diminuiu um pouco no período.")
 st.sidebar.markdown('Filtros para o gráfico')
 df_sul = df.loc[df['regiao'] == 'Sul']
 df_sul_91 = df_sul.loc[df_sul['ano'] == 1991]
@@ -85,3 +84,4 @@ fig_idh_reg.update_layout(
    yaxis_title="idhm"
    )
 st.plotly_chart(fig_idh_reg)
+st.markdown("O gráfico mostra a evolução dos valores médios de idhm das cinco regiões brasileiras. Fica evidente um processo de incremento dos valores de idh entre 1991 e 2010. Percebe-se também que os valores de idh nas regiões norte e nordeste são discretamente inferiores que os das demais regiões, da mesma maneira precebe-se que esta diferença diminuiu um pouco no período.")
