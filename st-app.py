14354ce99647101dd502ff5c089a78109f3c22a6
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-14 11:47:56 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -6,7 +6,7 @@ import PIL
 from PIL import Image
 import plotly.graph_objects as go
 
-
+st.set_page_config(page_title="Início", page_icon=":home:")
 st.title('IDH')
 texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel Duarte, Enio Kilder'
 st.text(texto1)
