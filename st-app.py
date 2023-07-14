f7f9008c5c9d119aec29b45b14a316ca21d82297
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-14 15:10:53 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -6,8 +6,17 @@ import PIL
 from PIL import Image
 import plotly.graph_objects as go
 
+fig_cesar = Image.open('assets/marca_cesar_school.png')
+fig_idh_composicao = Image.open('assets/idh_composicao.png')
+
 st.set_page_config(page_title="Início", page_icon=":home:")
 st.title('IDH')
-texto1 = 'Cesar School - Curso de Especialização em Engenharia e Análise de Dados\nProjeto Final da Disciplina Análise e Visualização de Dados\nEquipe 12: Arthur Perruci, Daniel Duarte, Enio Kilder'
-st.text(texto1)
+st.write('Cesar School - Curso de Especialização em Engenharia e Análise de Dados')
+st.write('Projeto Final da Disciplina Análise e Visualização de Dados')
+st.write('Equipe 12: Arthur Perruci, Daniel Duarte, Enio Kilder')
 
+col1, col2 = st.columns(2)
+with col1:
+  st.image(fig_cesar)
+with col2:
+  st.image(fig_idh_composicao)
