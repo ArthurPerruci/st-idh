d76cf628bd0eaeaf0f0fef08acb56199220c578b
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-17 10:38:49 -0300

    Update 4_IDH_Longevidade.py

--- pages/4_IDH_Longevidade.py
+++ pages/4_IDH_Longevidade.py
@@ -69,8 +69,8 @@ st.plotly_chart(fig_idh_long)
 st.plotly_chart(fig_idh_long_leg)
 st.markdown("Esperança de Vida ao Nascer e IDH")
 fig_idh_espvida = alt.Chart(df, title="Relação entre Esperança de vida ao Nascer e IDH - " + str(ano_grafico)).mark_circle().encode(
-     x=alt.X('espvida', scale=alt.Scale(domain=[50,90])),
-     y=alt.Y('idhm'),
+     x=alt.X('espvida', scale=alt.Scale(domain=[50,80])),
+     y=alt.Y('idhm', scale=alt.Scale(domain=[0,1])),
      color='regiao',
      ).interactive()
 st.altair_chart(fig_idh_espvida, theme="streamlit", use_container_width=True)
