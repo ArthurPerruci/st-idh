cf500f9a0ab1c7f95a1f6ee43d3d9e9181a55e73
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-18 14:08:11 -0300

    Update 4_IDH_Educação.py

--- pages/4_IDH_Educação.py
+++ pages/4_IDH_Educação.py
@@ -7,16 +7,6 @@ from PIL import Image
 import plotly.graph_objects as go
 import altair as alt
 from plotly.subplots import make_subplots
-from st_pages import Page, show_pages
-
-show_pages([
-  Page("st-app.py", "Início", ":house:"),
-  Page("pages/1_Entenda_o_Idh.py", "Entenda o IDH", ":thinking_face:"),
-  Page("pages/2_Evolução_do_Idh_nas_regioes.py", "Evolução do IDH nas Regiões", ":chart_with_upwards_trend:"),
-  Page("pages/3_IDH_Longevidade.py", "IDH - Longevidade", ":older_man:"),
-  Page("pages/4_IDH_Educação.py", "IDH - Educação", ":books:"),
-  Page("pages/5_IDH_Renda.py", "IDH - Renda", ":moneybag:"),
-  ])
 
 #Leitura do data frame
 df = pd.read_csv("data/atlas.csv")
