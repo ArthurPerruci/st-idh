7ac53c2b400419f63883e13e37c37ec4e51da530
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-18 13:54:01 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -10,14 +10,16 @@ from st_pages import Page, show_pages
 fig_cesar = Image.open('assets/marca_cesar_school.png')
 fig_idh_composicao = Image.open('assets/idh_composicao.png')
 
-show_pages([
+show_pages(
+  [
   Page("st-app.py", "Início", ":house:"),
   Page("pages/1_Entenda_o_Idh.py", "Entenda o IDH", ":thinking_face:"),
   Page("pages/2_Evolução_do_Idh_nas_regioes.py", "Evolução do IDH nas Regiões", ":chart_with_upwards_trend:"),
   Page("pages/3_IDH_Longevidade.py", "IDH - Longevidade", ":older_man:"),
   Page("pages/4_IDH_Educação.py", "IDH - Educação", ":books:"),
   Page("pages/5_IDH_Renda.py", "IDH - Renda", ":moneybag:"),
-  ])
+  ]
+)
 
 st.title('IDH')
 st.write('Cesar School - Curso de Especialização em Engenharia e Análise de Dados')
