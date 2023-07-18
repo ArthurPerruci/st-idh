0971c674960f692434666ef677e29d7fbd05116e
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-18 13:22:31 -0300

    Update 5_IDH_Renda.py

--- pages/5_IDH_Renda.py
+++ pages/5_IDH_Renda.py
@@ -5,15 +5,9 @@ import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
 import plotly.graph_objects as go
+from st_pages import show_pages_from_config
 
-show_pages([
-    Page("st-app.py", "Início"),
-    Page("pages/1_Entenda_o_Idh.py", "Entenda o IDH"),
-    Page("pages/2_Evolução_do_Idh_nas_regioes.py", "Evolução do IDH nas Regiões"),
-    Page("pages/3_IDH_Longevidade.py", "IDH - Longevidade"),
-    Page("pages/4_IDH_Educação.py", "IDH - Educação"),
-    Page("pages/5_IDH_Renda.py", "IDH - Renda"),
-  ])
+show_pages_from_config()
 
 #Leitura do data frame
 df = pd.read_csv("data/atlas.csv")
@@ -42,7 +36,6 @@ fig_ren_renpcap = Image.open('assets/fig_ren_renpcap.png')
 fig_ren_ag_ban = Image.open('assets/fig_ren_ag_ban.png')
 
 #Início da página
-#st.set_page_config(page_title="IDH Renda", page_icon=":coin:")
 st.title('IDH - Renda')
 
 #Colunas para as imagens
