3b20fd9f6213e894d8ac75e56dd3bd4e4231d128
ArthurPerruci <105933572+ArthurPerruci@users.noreply.github.com>
2023-07-18 13:18:10 -0300

    Update st-app.py

--- st-app.py
+++ st-app.py
@@ -5,21 +5,13 @@ import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
 import plotly.graph_objects as go
-from st_pages import Page, show_pages, add_page_title
+from st_pages import show_pages_from_config
 
 fig_cesar = Image.open('assets/marca_cesar_school.png')
 fig_idh_composicao = Image.open('assets/idh_composicao.png')
 
-show_pages([
-    Page("st-app.py", "Início", ":home:"),
-    Page("pages/1_Entenda_o_Idh.py", "Entenda o IDH", ":curious:"),
-    Page("pages/2_Evolução_do_Idh_nas_regioes.py", "Evolução do IDH nas Regiões", ":plot:"),
-    Page("pages/3_IDH_Longevidade.py", "IDH - Longevidade", ":old_man:"),
-    Page("pages/4_IDH_Educação.py", "IDH - Educação", ":books:"),
-    Page("pages/5_IDH_Renda.py", "IDH - Renda", ":coin:"),
-  ])
+show_pages_from_config()
 
-#st.set_page_config(page_title="Início", page_icon=":home:")
 st.title('IDH')
 st.write('Cesar School - Curso de Especialização em Engenharia e Análise de Dados')
 st.write('Projeto Final da Disciplina Análise e Visualização de Dados')
